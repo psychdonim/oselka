f9c4218d616c2209612e44ead691ec78fc31e4ae
Ivshin Artemij <ivshin.artemij@yandex.ru>
2026-07-10 15:27:01 +0500

    fix renaming

--- infrastructure/src/main/scala/db/postgres/config/PostgresConfig.scala
+++ infrastructure/src/main/scala/db/postgres/config/PostgresConfig.scala
@@ -20,14 +20,14 @@ object PostgresConfig {
     }
   }
 
-  private def load: Either[ConfigError, Config] = for {
+  private def load: Either[ConfigError, PostgresConfig] = for {
     host <- fromEnv("PSYD_OSELKA_POSTGRES_HOST")
     portStr <- fromEnv("PSYD_OSELKA_POSTGRES_PORT")
     port <- portStr.toIntOption.toRight(InvalidPort(portStr))
     database <- fromEnv("PSYD_OSELKA_POSTGRES_DB")
     user <- fromEnv("PSYD_OSELKA_POSTGRES_USER")
     password <- fromEnv("PSYD_OSELKA_POSTGRES_PASSWORD")
-  } yield Config(host, port.toInt, database, user, password)
+  } yield PostgresConfig(host, port.toInt, database, user, password)
 
   private def fromEnv(key: String): Either[ConfigError, String] =
     sys.env.get(key).toRight(MissingEnv(key))
